899ed13462f8ed59227ec374c2509756edc3a236
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-02-16 20:44:58 -0300

    Projeto criado

---
+++ src/test/java/com/gestao/financeira/projeto/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.financeira.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
