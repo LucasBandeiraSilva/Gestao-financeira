99fb67af2e268dbea93a3b78ca413463d095ad66
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-05-13 10:53:10 -0300

    adicionando mais campos para a validação de dados

--- src/main/resources/static/js/valida-senha-cliente.js
+++ src/main/resources/static/js/valida-senha-cliente.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("form");
 const campos = document.querySelectorAll(".required");
 const spans = document.querySelectorAll(".span-required");
+const validFeedback = document.querySelectorAll(".valid");
 let isInvalidComparePassword = true;
 let isInvalidPassword = true;
 
@@ -13,20 +14,19 @@ function removeError(index) {
   campos[index].classList.remove("is-invalid");
   spans[index].classList.remove("invalid-feedback");
   spans[index].style.display = "none";
+  campos[index].classList.add("is-valid");
+  validFeedback[index].classList.add("valid-feedback")
+  validFeedback[index].style.display="block"
 }
 
-function passwordValidate() {
-  if (campos[0].value.length < 6) {
+function nameValidation(){
+  if(campos[0].value.length < 3){
     setError(0);
-    isInvalidPassword = false;
-  } else {
-    removeError(0);
-    isInvalidPassword = true;
-  }
+  }else
+  removeError(0);
 }
-
-function comparePassword() {
-  if (campos[0].value.length !== campos[1].value.length) {
+function passwordValidate() {
+  if (campos[1].value.length < 6) {
     setError(1);
     isInvalidPassword = false;
   } else {
@@ -34,14 +34,23 @@ function comparePassword() {
     isInvalidPassword = true;
   }
 }
+
+function comparePassword() {
+  if (campos[1].value.length !== campos[2].value.length) {
+    setError(2);
+    isInvalidPassword = false;
+  } else {
+    removeError(2);
+    isInvalidPassword = true;
+  }
+}
 form.addEventListener("submit", (event) => {
-    if (isInvalidComparePassword && !isInvalidPassword) {
-      console.log("formulario não enviado");
-      console.log("senha boolean: " + isInvalidPassword);
-      console.log("compareSenha boolean: " + isInvalidComparePassword);
-      event.preventDefault();
-    }else{
-      console.log("form sem erros de senha")
-    }
-    
-  });
+  if (isInvalidComparePassword && !isInvalidPassword) {
+    console.log("formulario não enviado");
+    console.log("senha boolean: " + isInvalidPassword);
+    console.log("compareSenha boolean: " + isInvalidComparePassword);
+    event.preventDefault();
+  } else {
+    console.log("form sem erros de senha");
+  }
+});
